b131b5b85f4ca0670d84bfb47876de2c70b81d75
Blake Davies <blakedavies1992@gmail.com>
2017-06-17 20:01:44 +1000

    Add files via upload

---
+++ js/contactform.js
@@ -0,0 +1,22 @@
+$('#gform').submit(function(e) {
+	      e.preventDefault();
+	      $.ajax({
+	        url: "https://docs.google.com/a/blakedavies.net/forms/d/e/1FAIpQLSd9oGWcAL9fvnf-P7900RZyQMv7AkKtJZq2-iOdvgq4Q61onA/formResponse",
+	        data: $(this).serialize(),
+	        type: "POST",
+	        dataType: "xml",
+	        success: function(data) {
+	          console.log('Submission successful');
+	        },
+	        error: function(xhr, status, error) {
+	          console.log('Submission failed: ' + error);
+	        }
+	      });
+	    });
+	    $('#gform').on('submit', function(e) {
+	  		$('#gform *').fadeOut(0);
+	  		$('#contact-box *').fadeOut(0);
+	  		$('#gform').prepend(
+	    		'<div class="thank-you-box"><h3 class="thank-you">Thank-you.<h3><p class="thank-you">Your message has now been sent to Australian Merchandise Corporation.</p></div>'
+	    	);
+	  	});
